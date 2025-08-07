90319129e60c45e52fca9977105e5a4daf05e344
Chetanputhon <kumarchetanfriend@gmail.com>
2025-08-07 17:03:41 +0530

    Update llm_client.py

--- llm_client.py
+++ llm_client.py
@@ -16,7 +16,7 @@ Document content:
 {file_text[:4000]}
     """
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.2
     )
